0bfd0b26c5589c5242a95ad442053d6834efcf68
Kevin Huelsmann <kjh311@gmail.com>
2016-10-22 11:31:18 -0700

    trying to get ngroute to work

--- work_here/js/compiledJS/concat.js
+++ work_here/js/compiledJS/concat.js
@@ -1,10 +1,54 @@
-var app = angular.module('myApp',[]);
+var app = angular.module('myApp',['ngRoute']);
 
+
+
+
+// app.config(['$routeProvider',
+//   function($routeProvider) {
+//     $routeProvider.
+//        .when("/", {
+//         templateUrl : "work_here/views/index.html"
+//     })
+//     .when("/movies", {
+//         templateUrl : "/work_here/views/movies.html"
+//     })
+//     .when("/tv", {
+//         templateUrl : "work_here/views/tv.html"
+//     })
+//     .when("/stars", {
+//         templateUrl : "work_here/views/stars.html"
+//     });
+//     // $locationProvider.html5Mode(true);
+// }]);
+
+//   app.config(function ($routeProvider) {
+//     $routeProvider
+//        .when("/", {
+//         templateUrl : "work_here/views/index.html"
+//     })
+//     .when("/movies", {
+//         templateUrl : "work_here/views/movies.html"
+//     })
+//     .when("/tv", {
+//         templateUrl : "work_here/views/tv.html"
+//     })
+//     .when("/stars", {
+//         templateUrl : "work_here/views/stars.html"
+//     });
+// });
+
+
+
+
+// CONTROLLER
 app.controller('myCtrl', ['$scope', '$http', 'userService', function($scope, $http, userService) {
   $scope.name = 'Kevin';
 
+
+
 var root_url = "https://api.themoviedb.org/3/";
 $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userService.key;
+
 
   $http.get(root_url+"movie/188927?api_key="+userService.key+"").success(function(data) {
     $scope.movieStuff = data;
@@ -21,7 +65,6 @@ $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userServic
   });
 
 // Popular Stars:
-
   $http.get(root_url+"person/popular?api_key="+userService.key+"&language=en-US&page=1"+userService.key+"").success(function(data) {
     $scope.popularPeople = data;
   });
@@ -30,6 +73,50 @@ $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userServic
   $scope.imgPath = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
 
 }]);
+
+// NG-ROUTE
+//     app.config(['$routeProvider', function($routeProvider){
+
+// $routeProvider
+// .when('/', {
+//             templateUrl : "work_here/views/index.html"
+//         })
+// .when("/movies", {
+//         templateUrl : "/work_here/views/movies.html",
+//         controller: 'myCtrl'
+//     })
+// }]);
+
+
+app.config(['$locationProvider', '$routeProvider',
+ function($location, $routeProvider) {
+  // alert($location.path);
+    $routeProvider
+        .when('/', {
+            templateUrl : "work_here/views/index.html"
+        })
+        .when("/movies", {
+        templateUrl : "/work_here/views/movies.html",
+        controller: 'myCtrl'
+    })
+}]);
+
+// app.config(function($routeProvider, $locationProvider) {
+//   $routeProvider
+
+//   .when('/', {
+//     templateUrl : "/work_here/views/index.html"
+//         })
+//   .when("/movies", {
+//     templateUrl : "/work_here/views/movies.html",
+//     controller   : 'myCtrl'
+//   })
+
+
+
+//   // $locationProvider.html5Mode(true);
+
+// })
 
 app.factory('userService', [function(){
 
