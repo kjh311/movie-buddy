aef75c3f459088b9fb4c3c8f48f09eca1cb933ad
Kevin Huelsmann <kjh311@gmail.com>
2016-10-22 14:17:30 -0700

    ng route and partials work

--- work_here/js/controller.js
+++ work_here/js/controller.js
@@ -1,52 +1,9 @@
 var app = angular.module('myApp',['ngRoute']);
 
 
-
-
-// app.config(['$routeProvider',
-//   function($routeProvider) {
-//     $routeProvider.
-//        .when("/", {
-//         templateUrl : "work_here/views/index.html"
-//     })
-//     .when("/movies", {
-//         templateUrl : "/work_here/views/movies.html"
-//     })
-//     .when("/tv", {
-//         templateUrl : "work_here/views/tv.html"
-//     })
-//     .when("/stars", {
-//         templateUrl : "work_here/views/stars.html"
-//     });
-//     // $locationProvider.html5Mode(true);
-// }]);
-
-//   app.config(function ($routeProvider) {
-//     $routeProvider
-//        .when("/", {
-//         templateUrl : "work_here/views/index.html"
-//     })
-//     .when("/movies", {
-//         templateUrl : "work_here/views/movies.html"
-//     })
-//     .when("/tv", {
-//         templateUrl : "work_here/views/tv.html"
-//     })
-//     .when("/stars", {
-//         templateUrl : "work_here/views/stars.html"
-//     });
-// });
-
-
-
-
 // CONTROLLER
 app.controller('myCtrl', ['$scope', '$http', 'userService', function($scope, $http, userService) {
   $scope.name = 'Kevin';
-
-
-
-
 
 var root_url = "https://api.themoviedb.org/3/";
 $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userService.key;
@@ -54,6 +11,16 @@ $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userServic
 
   $http.get(root_url+"movie/188927?api_key="+userService.key+"").success(function(data) {
     $scope.movieStuff = data;
+  });
+
+// Latest movies
+$http.get(root_url+"movie/now_playing?api_key="+userService.key+"&language=en-US&page=1"+userService.key+"").success(function(data) {
+    $scope.nowPlayingMovies = data;
+  });
+
+// Top Rated movies
+  $http.get(root_url+"movie/top_rated?api_key="+userService.key+"&language=en-US&page=1"+userService.key+"").success(function(data) {
+    $scope.topMovies = data;
   });
 
 // Popular Movies:
@@ -77,29 +44,23 @@ $scope.movieId = "https://api.themoviedb.org/3/movie/188927?api_key="+userServic
 }]);
 
 // NG-ROUTE
-//     app.config(['$routeProvider', function($routeProvider){
-
-// $routeProvider
-// .when('/', {
-//             templateUrl : "work_here/views/index.html"
-//         })
-// .when("/movies", {
-//         templateUrl : "/work_here/views/movies.html",
-//         controller: 'myCtrl'
-//     })
-// }]);
-
-
 app.config(['$locationProvider', '$routeProvider',
  function($location, $routeProvider) {
   // alert($location.path);
     $routeProvider
         .when('/', {
-            templateUrl : "/work_here/views/index.html"
+            templateUrl : "/work_here/views/home.html"
         })
         .when("/movies", {
           templateUrl : "/work_here/views/movies.html"
-    })
+        })
+        .when("/tv", {
+          templateUrl : "/work_here/views/tv.html"
+        })
+        .when("/stars", {
+          templateUrl : "/work_here/views/stars.html"
+        })
+        .otherwise({redirectTo:'/'});
 }]);
 
 
