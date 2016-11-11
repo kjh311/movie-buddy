e18603370f242068a9b3469c8e488750f3f75331
Kevin Huelsmann <kjh311@gmail.com>
2016-11-11 15:47:45 -0800

    woking

--- work_here/js/write_js_here.js
+++ work_here/js/write_js_here.js
@@ -15,46 +15,39 @@ $('.movie-link').click(function() {
   $('.movie-link').addClass('white');
   $('.star-link').removeClass('white');
   $('.tv-link').removeClass('white');
+  $('.news-link').removeClass('white');
 });
 
 $('.star-link').click(function() {
   $('.star-link').addClass('white');
   $('.movie-link').removeClass('white');
   $('.tv-link').removeClass('white');
+  $('.news-link').removeClass('white');
 });
 
 $('.tv-link').click(function() {
   $('.tv-link').addClass('white');
   $('.star-link').removeClass('white');
   $('.movie-link').removeClass('white');
+  $('.news-link').removeClass('white');
 });
 
-// $( '.readMore' ).click(function() {
-
-//               $( '.readMoreLink' ).hide();
-
-
-//     });
+$('.news-link').click(function() {
+  $('.news-link').addClass('white');
+  $('.tv-link').removeClass('white');
+  $('.star-link').removeClass('white');
+  $('.movie-link').removeClass('white');
+});
 
 
 $('.specific-link').click(function() {
   $('.star-link').removeClass('white');
   $('.movie-link').removeClass('white');
   $('.tv-link').removeClass('white');
+  $('.news-link').removeClass('white');
 });
 
-// // if ( !$("#productInfoGrid").has("img") ) {
-//     $(this).hide();
-// }
 
-// if ( $(".similar-movie-li-1 img").find('img').length == 0);​
-//     $('.slides3').hide();
-// }
-
-  // // Initialize collapse button
-  // $(".button-collapse").sideNav();
-  // // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  // // $('.collapsible').collapsible();
 
    $('.button-collapse').sideNav({
       menuWidth: 300, // Default is 240
