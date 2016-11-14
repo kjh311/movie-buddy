fa2015469ebb491fc3433a94792bfbd5b1965ef3
Kevin Huelsmann <kjh311@gmail.com>
2016-11-14 08:13:29 -0800

    address

--- work_here/js/compiledJS/concat.js
+++ work_here/js/compiledJS/concat.js
@@ -324,28 +324,28 @@ app.config(['$locationProvider', '$routeProvider',
           controller: 'myCtrl'
         })
         .when("/movies", {
-          templateUrl : "movie-buddy/work_here/views/movies.html"
+          templateUrl : "/movie-buddy/work_here/views/movies.html"
         })
         .when('/movie/:id', {
-          templateUrl  : 'movie-buddy/work_here/views/moviePage.html',
+          templateUrl  : '/movie-buddy/work_here/views/moviePage.html',
           controller: 'movieController'
         })
         .when("/tv", {
-          templateUrl : "movie-buddy/work_here/views/tv.html"
+          templateUrl : "/movie-buddy/work_here/views/tv.html"
         })
         .when('/tv/:id', {
-          templateUrl  : 'movie-buddy/work_here/views/tvPage.html',
+          templateUrl  : '/movie-buddy/work_here/views/tvPage.html',
           controller: 'tvController'
         })
         .when("/stars", {
-          templateUrl : "movie-buddy/work_here/views/stars.html"
+          templateUrl : "/movie-buddy/work_here/views/stars.html"
         })
         .when('/person/:id', {
-          templateUrl  : 'movie-buddy/work_here/views/starPage.html',
+          templateUrl  : '/movie-buddy/work_here/views/starPage.html',
           controller: 'starController'
         })
         .when('/news', {
-          templateUrl  : 'movie-buddy/work_here/views/news.html'
+          templateUrl  : '/movie-buddy/work_here/views/news.html'
         })
         .otherwise({redirectTo:'/'});
 }]);
