4806a42b852c00c3affa6bafca8f1b17f3a2151b
Kevin Huelsmann <kjh311@gmail.com>
2016-11-14 16:45:35 -0800

    background image

--- work_here/js/compiledJS/concat.js
+++ work_here/js/compiledJS/concat.js
@@ -415,4 +415,5 @@ $('.button-collapse').sideNav({
   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
   draggable: true // Choose whether you can drag to open on touch screens
 });
+$('.parallax').parallax();
 });
